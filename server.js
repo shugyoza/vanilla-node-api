fba27235be8d0db14e559f17905e117a819b75d2
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-12 22:18:48 -0700

    did the callback in the http.createServer(... to print hello world in the browser on whatever end point.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
-
-const server = http.createServer((req, res) => {});
+// Prints Hello World in the browser
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/html")
+    res.write("<h1>Hello World</h1>")
+    res.end;
+});
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
