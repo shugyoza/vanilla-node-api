7f5193ba3fe0810efd988ccb21b7d6f2f09d9295
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-12 22:26:51 -0700

    rewrite the callback for server to display the json products.

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const http = require("http");
-const products = require("./data/products")
+const products = require("./data/products");
 
-// Prints Hello World in the browser
+/*
+// 1. Prints Hello World in the browser
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-type", "text/html")
     res.write("<h1>Hello World</h1>")
     res.end;
 });
+*/
+
+// 2.
+const server = http.createServer((req, res) => {
+    // summarize res.statusCode and res.setHeader together
+    res.writeHead(200, {"Content-type": "application/json"});
+    // will come out with just a regular js array, thus we have to stringify
+    res.write(JSON.stringify(products));
+    res.end;
+})
+
+
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
