f816e8b81787f902dc1874959cdfcab316a51e9e
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-13 10:09:57 -0700

    defined findById(id) getProduct(req, res, id), exports imports, and added if else statement for GET /api/products/:id

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,6 +12,24 @@ async function getProducts(req, res) {
     }
 }
 
+// 12 define and export for /api/products/id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.writeHead(400, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({message: "Product Not Found"}))
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(product))
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
